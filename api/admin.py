e3ac5eec5802683b347385c0106d4c07f3f490db
Apeksha <apeksha.2018562@iit.ac.lk>
2020-08-27 13:30:02 +0530

    new changes

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Adtypes
 
-# Register your models here.
+admin.site.register(Adtypes)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 class Adtypes(models.Model):
-    type_id = models.AutoField(primary_key=True)
+    type_id = models.IntegerField(primary_key=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     type_name = models.CharField(max_length=50)
-    Is_deleted = models.BooleanField(default=True)
+    Is_deleted = models.BooleanField()
