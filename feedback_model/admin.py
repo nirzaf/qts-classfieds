66491db26b6a067119187eb20fc3abc4582ac3d2
Farook M Fazrin <mfmfazrin1986@gmail.com>
2020-08-30 15:18:09 +0530

    Code Cleanups new

--- feedback_model/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Feedback
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Feedback)
--- feedback_model/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeedbackModelConfig(AppConfig):
-    name = 'feedback_model'
