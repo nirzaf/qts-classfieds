bdb678b064b1ad299194ce487a9aeadf5f5d2c62
Farook M Fazrin <nirzaf1@hotmail.com>
2020-09-08 09:08:58 +0530

    Fix the linking structure issues

--- qts_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GenericAdTypeAPI, GenericAdTypeGetAPI
-
-urlpatterns = [
-    path('api/ad_types', GenericAdTypeGetAPI.as_view()),
-    path('api/ad_types/<int:pk>/', GenericAdTypeAPI.as_view())
-]
--- qts_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('qts_api.urls')),
-    path('', admin.site.urls)
-]
