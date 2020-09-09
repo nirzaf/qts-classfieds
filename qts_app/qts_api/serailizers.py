cd20182de1ae6d0e2d97712fbed3c41e6db79ffb
Farook M Fazrin <nirzaf1@hotmail.com>
2020-09-09 18:09:13 +0530

    changes

--- qts_app/qts_api/serailizers.py
+++ qts_app/qts_api/serailizers.py
@@ -5,6 +5,5 @@ from .models import ad_type
 class ad_typeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ad_type
-        # fields = '__all__'
-        fields = ['type_id', 'type_name']
+        fields = '__all__'
 
