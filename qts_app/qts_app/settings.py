16c50b2a869acc81082ff32418241fba24286f47
pankajan05 <pankajan05@gmail.com>
2020-10-12 17:29:33 +0530

    add rest framework

--- qts_app/qts_app/settings.py
+++ qts_app/qts_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eb36^5m4g&c%$wu9gnt1zqpmz@^4bjl!wo-q^-u&nywjrgs-aa'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
