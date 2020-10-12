7ac3fac2d39f5f575b94503c976dbbf484d9afb4
pankajan05 <pankajan05@gmail.com>
2020-10-12 11:44:03 +0530

    update static root

--- qts_app/qts_app/settings.py
+++ qts_app/qts_app/settings.py
@@ -106,5 +106,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
