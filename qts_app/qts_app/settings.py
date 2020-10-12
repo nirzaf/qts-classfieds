9746b24b19690b1229afe734870556b17a18127a
pankajan05 <pankajan05@gmail.com>
2020-10-12 14:49:04 +0530

    add django_heroku

--- qts_app/qts_app/settings.py
+++ qts_app/qts_app/settings.py
@@ -1,6 +1,7 @@
+import django_heroku
+import os
 from pathlib import Path
 
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+#Activate django-heroku
+django_heroku.settings(locals())
